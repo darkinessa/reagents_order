28a0f781f1465dea233c16598a018de93bcc191b
nneuro <nneuro2013@gmail.com>
2019-03-30 23:35:00 +0300

    added init&routes

---
+++ app/routes.py
@@ -0,0 +1,26 @@
+from app import app
+from app.forms import LoginForm
+from flask import flash, redirect, render_template, url_for
+from flask_wtf import FlaskForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Вход в личный кабинет', form=form)
+
+
+@app.route('/submit', methods=('GET', 'POST'))
+def submit():
+    form = MyForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('submit.html', form=form)
