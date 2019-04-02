16b39fb1442c27d5046893c8895f54cd44bd453a
Irina Koroleva <korolevai@gmail.com>
2019-04-02 13:11:08 +0300

    created registration, updated forms, routes, models and html templates

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,34 @@
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField,  StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import Email, EqualTo, DataRequired, ValidationError
+from app.models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Логин', validators=[DataRequired()])
-    password = PasswordField('Пароль', validators=[DataRequired()])
-    remember_me = BooleanField('Запомнить')
-    submit = SubmitField('Войти')
+    username = StringField('Логин', validators=[DataRequired()], render_kw={"class": "form_control"})
+    password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class": "form_control"})
+    remember_me = BooleanField('Запомнить', render_kw={"class": "form-check-label"})
+    submit = SubmitField('Войти', render_kw={"class": "form-check-label"})
 
-# class LoginForm(FlaskForm):
-#     username = StringField('Имя пользователя', validators=[DataRequired()], render_kw={"class": "form-control"})
-#     password = PasswordField('Пароль', validators=[DataRequired()], render_kw={"class": "form-control"})
-#     submit = SubmitField('Отправить', render_kw={"class": "btn btn-primary"})
+class RegistrationForm(FlaskForm):
+    username = StringField('Введите Ваш логин', validators=[DataRequired()])
+    email = StringField('Введите Ваш email', validators=[DataRequired(), Email()])
+    password = PasswordField('Введите пароль', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Повторите введеный пароль', validators=[DataRequired(), EqualTo('password')])
+    name = StringField('Ведите имя и фамилию', validators=[DataRequired()])
+    phone_number = StringField('Введите свой контакнтый телефон', validators=[DataRequired()])
+    position = StringField('Должность')
+    laboratory = StringField('Лаборатория')
+    supervisor = StringField('Научный руководитель')
+    submit = SubmitField('Зарегистрироваться', render_kw={"class": "form-check-label"})
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Данный логин занят, используйте другой')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Данный email уже используется, введите другой')
+
--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,56 @@
-from app import app
-from app.forms import LoginForm
-from flask import flash, redirect, render_template, url_for
+from app import app, db
+from app.forms import LoginForm, RegistrationForm
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import current_user, login_user, logout_user, login_required
+from werkzeug.urls import url_parse
+
+from app.models import User
+
 
 @app.route('/')
 @app.route('/index')
+#@login_required
 def index():
     return render_template('index.html', title='Home')
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Неверный логин или пароль')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Вход в личный кабинет', form=form)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+@app.route('/registration', methods=['GET', 'POST'])
+def registration():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data,
+                    name=form.name.data, phone_number=form.phone_number.data,
+                    supervisor=form.supervisor.data, possition=form.position.data, laboratory=form.laboratory.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Вы успешно зарегистрировались')
+        return redirect(url_for('login'))
+    return render_template('registration.html', title='Регистрация', form=form)
+
